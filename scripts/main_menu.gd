f494a8177521fd43952da0ebce064edbbec74270
r.chel.i <rachelqi.rq@gmail.com>
2026-01-17 05:29:52 -0500

    Create main menu & start game

---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var title = $Title
+@onready var play_button = $PlayButton
+
+# Start game when user presses play
+func _on_play_pressed():
+	play_button.disabled = true
+
+	var tween = create_tween()
+	tween.set_parallel(true)
+
+	tween.tween_property(title, "modulate:a", 0.0, 0.5)
+	tween.tween_property(play_button, "modulate:a", 0.0, 0.5)
+
+	tween.finished.connect(func():
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
+	)
